2cdcf320068d18adb04996691a2aca514c8a4c03
haima1998 <zhuoyinga@163.com>
2020-05-17 19:12:32 +0800

    add resize screen shot

---
+++ zyrun_resize_screenshot.sh
@@ -0,0 +1,9 @@
+python 13_resize_image.py 1080 1920 $1 $2
+python 13_resize_image.py 1125 2436 $1 $2
+python 13_resize_image.py 1242 2208 $1 $2
+python 13_resize_image.py 1242 2688 $1 $2
+python 13_resize_image.py 1536 2048 $1 $2
+python 13_resize_image.py 2048 2732 $1 $2
+python 13_resize_image.py 640 1136  $1 $2
+python 13_resize_image.py 640 960   $1 $2
+python 13_resize_image.py 750 1334  $1 $2
