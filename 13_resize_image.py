97b93b55e0eb403e2207ac7a3ef50179d0494459
haima1998 <zhuoyinga@163.com>
2020-05-17 12:11:14 +0800

    add resize imnage

---
+++ 13_resize_image.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+
+import datetime
+import json
+import os
+import re
+import fnmatch
+from PIL import Image
+import numpy as np
+import json
+from lxml.etree import Element, SubElement, tostring
+from xml.dom.minidom import parseString
+import cv2
+import xml.etree.ElementTree as ET
+from PIL import Image
+import time
+import sys
+
+# DEST_DIR = '/home/charlie/Pictures/output'
+# INPUT_IMAGE_FILE = '/home/charlie/Pictures/appicon_1024.png'
+
+
+
+def main():
+            output_x = int(sys.argv[1])
+            output_y = int(sys.argv[2])
+            input_file_name = sys.argv[3]
+            output_dir = sys.argv[4]
+
+            image_filename = input_file_name
+
+            (filepath, tempfilename) = os.path.split(image_filename)
+            (filename, extension) = os.path.splitext(tempfilename)
+
+            new_file_name = filename + '_' + str(output_x) + '_' + str(output_y) + '.jpg'
+            save_file_name = os.path.join(output_dir, new_file_name)
+            print(new_file_name)
+
+            #A .read source image file
+            image = cv2.imread(image_filename)
+
+            #B. resize
+            img_dst = cv2.resize(image, (output_x, output_y))
+
+            #D. save result image to file
+            cv2.imwrite(save_file_name, img_dst)
+
+if __name__ == "__main__":
+    main()
+
+
+
